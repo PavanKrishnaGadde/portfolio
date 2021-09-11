f03aaa52037afb8a3d0daf2993181b703837e3d3
PavanKrishnaGadde <pavankrishna1729@gmail.com>
2021-09-11 17:43:42 +0530

    inital css changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+import ProfileCard from './components/ProfileCard'
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <div className="profile-section">
+         <ProfileCard />
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/components/ProfileCard.js
@@ -0,0 +1,12 @@
+import '../styles/profileCard.scss';
+
+function ProfileCard() {
+  return (
+    <div className="card-section">
+      <strong>Pavan Krishna Gadde</strong>
+      <div>Full Stack Developer</div>
+    </div>
+  );
+}
+
+export default ProfileCard;
